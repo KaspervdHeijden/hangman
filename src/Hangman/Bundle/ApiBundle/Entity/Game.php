daf4f949553f434be2a0617dd381b1b802836e86
Kasper van der Heijden <kasper@kaspervdheijden.nl>
2017-11-14 00:02:46 +0100

    Update Game.php

--- src/Hangman/Bundle/ApiBundle/Entity/Game.php
+++ src/Hangman/Bundle/ApiBundle/Entity/Game.php
@@ -28,7 +28,7 @@ class Game
     const STATUS_BUSY = 'busy';
     const STATUS_FAIL = 'fail';
 
-	/**
+    /**
      * @var int
      * 
      * @ORM\Column(name="id", type="integer")
@@ -37,33 +37,33 @@ class Game
      */
     private $id;
 
-	/**
-	 * @var int
-	 * 
-	 * @ORM\Column(name="tries_left", type="integer")
-	 */
-	private $triesLeft = self::MAX_TRIES;
+    /**
+     * @var int
+     * 
+     * @ORM\Column(name="tries_left", type="integer")
+     */
+    private $triesLeft = self::MAX_TRIES;
 
-	/**
-	 * @var string
-	 * 
-	 * @ORM\Column(name="word", type="string", length=255)
-	 */
-	private $word;
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="word", type="string", length=255)
+     */
+    private $word;
 
-	/**
-	 * @var string
-	 * 
-	 * @ORM\Column(name="characters_guessed", type="json_array")
-	 */
-	private $characters_guessed = [];
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="characters_guessed", type="json_array")
+     */
+    private $characters_guessed = [];
 
-	/**
-	 * @var string
-	 * 
-	 * @ORM\Column(name="status", length=255)
-	 */
-	private $status = self::STATUS_BUSY;
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="status", length=255)
+     */
+    private $status = self::STATUS_BUSY;
 
     /**
      * Get the id.
@@ -75,133 +75,133 @@ class Game
         return $this->id;
     }
 
-	/**
-	 * Gets the number of tries left.
-	 * 
-	 * @return int
-	 */
-	public function getTriesLeft() : int
-	{
-		return $this->triesLeft;
-	}
+    /**
+     * Gets the number of tries left.
+     * 
+     * @return int
+     */
+    public function getTriesLeft() : int
+    {
+        return $this->triesLeft;
+    }
 
-	/**
-	 * Decrement the number of tries left.
-	 * 
-	 * @return Game
-	 */
-	public function decrementTriesLeft() : Game
-	{
-		--$this->triesLeft;
-		return $this;
-	}
+    /**
+     * Decrement the number of tries left.
+     * 
+     * @return Game
+     */
+    public function decrementTriesLeft() : Game
+    {
+        --$this->triesLeft;
+        return $this;
+    }
 
-	/**
-	 * Get the word.
-	 * 
-	 * @return string
-	 */
-	public function getWord() : string
-	{
-		return $this->word;
-	}
+    /**
+     * Get the word.
+     * 
+     * @return string
+     */
+    public function getWord() : string
+    {
+        return $this->word;
+    }
 
-	/**
-	 * Set the word.
-	 * 
-	 * @param string $word
-	 * @return Game
-	 */
-	public function setWord(string $word) : Game
-	{
-		$this->word = $word;
-		return $this;
-	}
+    /**
+     * Set the word.
+     * 
+     * @param string $word
+     * @return Game
+     */
+    public function setWord(string $word) : Game
+    {
+        $this->word = $word;
+        return $this;
+    }
 
-	/**
-	 * Get the guessed characters.
-	 * 
-	 * @return string[]
-	 */
-	public function getCharacters_guessed() : array
-	{
-		return $this->characters_guessed;
-	}
+    /**
+     * Get the guessed characters.
+     * 
+     * @return string[]
+     */
+    public function getCharacters_guessed() : array
+    {
+        return $this->characters_guessed;
+    }
 
-	/**
-	 * Adds a character to the guessed characters list.
-	 * 
-	 * @param string $character
-	 * @return Game
-	 */
-	public function addGuessedCharacter(string $character) : Game
-	{
-		if (!$this->isCharacterAlreadyGuessed($character)) {
-			$this->characters_guessed[] = $character;
-		}
+    /**
+     * Adds a character to the guessed characters list.
+     * 
+     * @param string $character
+     * @return Game
+     */
+    public function addGuessedCharacter(string $character) : Game
+    {
+        if (!$this->isCharacterAlreadyGuessed($character)) {
+            $this->characters_guessed[] = $character;
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get the game status (busy/fail/succes)
-	 * 
-	 * @see Game::STATUS_BUSY
-	 * @see Game::STATUS_FAIL
-	 * @see Game::STATUS_SUCCESS
-	 * @return string
-	 */
-	public function getStatus() : string
-	{
-		return $this->status;
-	}
+    /**
+     * Get the game status (busy/fail/succes)
+     * 
+     * @see Game::STATUS_BUSY
+     * @see Game::STATUS_FAIL
+     * @see Game::STATUS_SUCCESS
+     * @return string
+     */
+    public function getStatus() : string
+    {
+        return $this->status;
+    }
 
-	/**
-	 * Sets the game status.
-	 * 
-	 * @param string $status
-	 * @return Game
-	 */
-	public function setStatus(string $status) : Game
-	{
-		$this->status = $status;
-		return $this;
-	}
+    /**
+     * Sets the game status.
+     * 
+     * @param string $status
+     * @return Game
+     */
+    public function setStatus(string $status) : Game
+    {
+        $this->status = $status;
+        return $this;
+    }
 
-	/**
-	 * Determines if a character was already guessed.
-	 * 
-	 * @param string $character
-	 * @return bool
-	 */
-	public function isCharacterAlreadyGuessed(string $character) : bool
-	{
-		return in_array($character, $this->getCharacters_guessed());
-	}
+    /**
+     * Determines if a character was already guessed.
+     * 
+     * @param string $character
+     * @return bool
+     */
+    public function isCharacterAlreadyGuessed(string $character) : bool
+    {
+        return in_array($character, $this->getCharacters_guessed());
+    }
 
-	/**
-	 * Gets the version of the word with all non-guessed characters obvuscated.
-	 * 
-	 * @see Game::REPLACE_CHAR
-	 * @return string The obvuscated word.
-	 */
-	public function getWordObvuscated() : string
-	{
-		$characters_guessed = $this->getCharacters_guessed();
-		$word = $this->getWord();
-		$len = strlen($word);
-		$result = '';
+    /**
+     * Gets the version of the word with all non-guessed characters obvuscated.
+     * 
+     * @see Game::REPLACE_CHAR
+     * @return string The obvuscated word.
+     */
+    public function getWordObvuscated() : string
+    {
+        $characters_guessed = $this->getCharacters_guessed();
+        $word = $this->getWord();
+        $len = strlen($word);
+        $result = '';
 
-		for ($i = 0; $i < $len; ++$i) {
-			$char = $word[$i];
+        for ($i = 0; $i < $len; ++$i) {
+            $char = $word[$i];
 
-			if (in_array($char, $characters_guessed)) {
-				$result .= $char;
-			} else {
-				$result .= self::REPLACE_CHAR;
-			}
-		}
+            if (in_array($char, $characters_guessed)) {
+                $result .= $char;
+            } else {
+                $result .= self::REPLACE_CHAR;
+            }
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 }
